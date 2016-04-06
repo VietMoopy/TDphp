b226ce68b23dbd2ed7bc6921f3b7cbca5a5b49ea
alexandrenguyen68817 <alexandrenguyen68817@gmail.com>
2016-04-06 06:28:59 -0400

    creation exo bookmarks

---
+++ bookmarks.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Ceci est une page de test avec des balises PHP</title>
+  <meta charset="utf-8" />
+  <style>
+    .rouge
+    {
+      background-color: red;    
+    }
+  </style>
+</head>
+
+<body>
+  <h2>Page de test</h2>
+
+  <p>
+    Cette page contient du code HTML avec des balises PHP.<br />
+    Voici quelques petits tests :
+  </p>
+
+<table border = 1>
+<thead><tr><th>Bookmarks</th></tr></thead>
+<tbody>
+ <tr>
+   <td><b>Site</b></td>
+  <td><b>Journaux</b></td>
+ </tr>
+<?php $repertoire = array("Moteurs" => array(("Google" => ("http://www.google.fr", image)),array("Yahoo"=> array("http://www.yahoo.fr",image))), 
+                          "Journaux" => array(("LeMonde" => ("http://www.lemonde.fr", image)),array("Linux"=> array("http://www.editions-diamond.fr/category/gnu-linux-magazine",image))), 
+                          "Sports" => array(("FFF" => ("http://www.fff.fr", image)),array("Eurosport"=> array("http://www.eurosport.fr",image))) ?>
+<?php foreach ($data as $value) : ?>
+  <?php $imc = $value["Poids"]/(($value["Taille"]/100)*($value["Taille"]/100)); $imc=round($imc,2); if($imc > 25){ ?>
+    <tr class = "rouge">
+  <?php } ?>
+  <?php $imc = $value["Poids"]/(($value["Taille"]/100)*($value["Taille"]/100)); $imc=round($imc,2); if($imc <= 25){ ?>
+    <tr>
+  <?php } ?>
+  <?php foreach($value as $valeur) : ?>
+    <td><?= htmlspecialchars($valeur) ?></td>
+  <?php endforeach ?>
+  </tr>
+<?php endforeach ?>
+</tbody>
+</table>
+</body>
+
+</html>     
