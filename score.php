1372a0fb7b4e309059b307a558fa4f776e91572f
alexandrenguyen68817 <alexandrenguyen68817@gmail.com>
2016-04-13 06:09:39 -0400

    Prise en compte des non reponses

--- score.php
+++ score.php
@@ -1,17 +1,27 @@
 <?php
 extract($_POST);
 $score =  0;
+$compteur = 0;
+
 for($i = 1; $i <= $nbq; $i++){
-  $reponse =  $_POST['question'.$i];
+    if (isset($_POST['question'.$i]))
+$reponse = $_POST['question'.$i];
+else
+$reponse = "";
   if($reponse == "vrai"){
     echo ("Question ".$i.": Bonne reponse");
     echo "<br>";
     $score++;
+  }
+  else if($reponse == ""){
+    echo ("Question ".$i.": Pas repondu");
+    echo "<br>";
   }
   else{
     echo ("Question ".$i.": Mauvaise reponse");
     echo "<br>";
   }
 }
+
 echo ("Score : ".$score);
 ?>
