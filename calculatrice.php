8facf1f06812768ac566ebc810c0777cf6718fab
alexandrenguyen68817 <alexandrenguyen68817@gmail.com>
2016-04-13 05:31:09 -0400

    Division par 0 impossible ajoute

--- calculatrice.php
+++ calculatrice.php
@@ -17,7 +17,7 @@ $op1 = $_POST['op1'];
 else
 $op1 = "";
     if (isset($_POST['op2']))
-$op2 = $_POST['op1'];
+$op2 = $_POST['op2'];
 else
 $op2 = "";
   if($op1 != "" && $op2 != ""){
@@ -35,16 +35,22 @@ $op2 = "";
                           echo $res;                
                 break;
                 case "division" :
-                          $res = $op1 / $op2;
-                          echo $res;                
+                  if($op2 == 0){
+                        echo "Division par 0 impossible !";
+                  }
+                else{
+                       $res = $op1 / $op2;
+                       echo $res;   
+                }
+
                 break;
   }
   }
     
  ?>
   <form method="post" action="calculatrice.php">
-    <input type="text" name="op1" id="op1" placeholder="Operand1" value=<?php if (isset($_POST['op1'])) echo $_POST['op1']; else echo "" ?>>
-    <select name ="signe">
+    <input type="text" name="op1" id="op1" placeholder="Operand1" value=<?php if (isset($_POST[ 'op1'])) echo $_POST[ 'op1']; else echo "" ?>>
+    <select name="signe">
   <option value="plus">+</option>
   <option  value="moins">-</option>
   <option  value="multiplication">x</option>
